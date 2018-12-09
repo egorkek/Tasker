cfa0527b82c612d96d113879c9e60bd369b50fc3
Unknown <patron6813@gmail.com>
2018-12-10 02:53:57 +0300

    First Commit

---
+++ build/precache-manifest.cd5f12385a7c52e9e108e17c009d8ea4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3c20487bf50f5c49845ecaa2f1e4be58",
+    "url": "/static/media/bg1.3c20487b.png"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "651429cca50ff8d7d553",
+    "url": "/static/js/main.651429cc.chunk.js"
+  },
+  {
+    "revision": "166ec06de3b079f833fe",
+    "url": "/static/js/1.166ec06d.chunk.js"
+  },
+  {
+    "revision": "651429cca50ff8d7d553",
+    "url": "/static/css/main.a0e4eb09.chunk.css"
+  },
+  {
+    "revision": "8b00876df951b1db12a1850a35392f52",
+    "url": "/index.html"
+  }
+];
